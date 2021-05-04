587ebc0646eb76859a3e2c8850385cd8e44dad7f
Avinash <avi1299@gmail.com>
2021-05-04 15:11:01 +0530

    Modified the Readme and Run_Simulation.sh script

--- README.md
+++ README.md
@@ -15,4 +15,18 @@ The ```PDB_Downloader.sh``` is a bash script that downloads the PDB for a molecu
 
 ```./PDB_Downloader.sh 1rnk```
 
+This creates a folder 1rnk and in it downloads the 1rnk.pdb from the RCSB Protein Data Bank.
+
+You will also need to create an ```input``` script containing the simulation paramters and the names of the input anf output files. Sample input scripts can be found in the oxDNA/EXAMPLES folder.
+
+Once you have the ```input``` file in the same directory as your PDB file, run the ```Run_Simulation.sh``` bash script and provide the name of the RNA molecule you wanted to simulate. For example
+
+```./Run_Simulation.sh 1rnk```
+
+The ```Run_Simulation.sh``` performs 5 steps:
+1. Creation of topology and configuration files from the PDB
+2. Visualizing the input files
+3. Running the simulation
+4. Visualizing the simulation
+5. Performing analysis on the output files of the simulation
 
--- Run_Simulation.sh
+++ Run_Simulation.sh
@@ -6,20 +6,24 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-cd $1
-
+cd $1 || (echo "Folder not present" && exit 1)
+#Step 1: Creation of topology and configuration files from the PDB
 echo "Creating the topology and initial configuration file from the PDB. 53 indicates chain is from 5' -> 3'"
-python3 ../tacoxDNA/src/PDB_oxDNA.py $1.pdb 53
+python3 ../tacoxDNA/src/PDB_oxDNA.py $1.pdb 53 
 
+#Step 2: Visualizing the input files
 echo "Visulaizing the topology and initial configuration"
 ./../cogli2/build/bin/cogli2 --drums --rna --always-centre -t $1.pdb.top $1.pdb.oxdna 
 
+#Step 3: Running the simulation
 echo "Running the simulation. Edit inputMD1 to change the simulation parameters"
 ./../oxDNA/build/bin/oxDNA input
 
+#Step 4: Visualizing the simulation
 echo "Visualizing the simulation. Use '+' and '-' keys to move the simulation forward and backward"
 ./../cogli2/build/bin/cogli2 --drums --rna --always-centre -t $1.pdb.top trajectory.dat
 
+#Step 4: Performing analysis on the output files of the simulation
 echo "Plotting the RoG throughout the simulation"
 python3 ../Analysis/RoG.py $1.pdb.top trajectory.dat
 
